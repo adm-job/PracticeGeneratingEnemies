e71a75e11c340b67798ba6107e0ccfddb8e5e6a9
Stanislav <adm.job@yandex.ru>
2026-02-18 02:16:53 +0500

    Update SpawnEnemy.cs

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class SpawnEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
