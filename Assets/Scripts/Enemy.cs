a8a1e880805270af564fe8317cf3c0ee95122616
Stanislav <adm.job@yandex.ru>
2026-02-18 11:39:23 +0500

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody), typeof(CapsuleCollider))]
 public class Enemy : MonoBehaviour
 {
 
