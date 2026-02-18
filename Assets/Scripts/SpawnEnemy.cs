48968921ee5775b8099df08610b98bdcbfa1f8e5
Stanislav <adm.job@yandex.ru>
2026-02-18 12:15:05 +0500

    Update SpawnEnemy.cs

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -1,8 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class SpawnEnemy : MonoBehaviour
 {
+    [SerializeField] private Enemy _enemy;
+    [SerializeField] private int _poolCapacity = 20;
+    [SerializeField] private int _poolMaxSize = 20;
+    [SerializeField] private float _repeatRate = 2f;
 
+    private ObjectPool<Enemy> _pool;
+
+    private void Awake()
+    {
+        _pool = new ObjectPool<Enemy>
+            (
+            createFunc: () => Instantiate(_enemy),
+            actionOnGet: (enemy) => GetOnAction(enemy),
+            actionOnRelease: (enemy) => enemy.Deactivate(),
+            actionOnDestroy: (enemy) => Destroy(enemy.gameObject),
+            collectionCheck: true,
+            defaultCapacity: _poolCapacity,
+            maxSize: _poolMaxSize
+            );
+
+    }
+
+    private void GetOnAction(Enemy enemy)
+    {
+        enemy = enemy;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(StartCreation());
+    }
+
+    private void ReleaseEnemy(Enemy enemy)
+    {
+
+    }
+
+    private IEnumerator StartCreation()
+    {
+        yield return new WaitForSeconds(_repeatRate);
+    }
 }
