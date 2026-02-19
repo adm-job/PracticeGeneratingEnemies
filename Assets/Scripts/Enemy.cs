a6d3d762beafe3664a9916d07fc906b52add6c89
Stanislav <adm.job@yandex.ru>
2026-02-19 15:55:40 +0500

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,17 +1,7 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody), typeof(CapsuleCollider))]
 public class Enemy : MonoBehaviour
 {
-    public Rigidbody Rigidbody {  get; private set; }
-    public CapsuleCollider Capsule { get; private set; }
-
-    private void Awake()
-    {
-        Rigidbody = GetComponent<Rigidbody>();
-        Capsule = GetComponent<CapsuleCollider>();
-    }
-    
     private void Update()
     {
         transform.position += transform.right * Time.deltaTime;
