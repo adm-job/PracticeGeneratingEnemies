132fa9298f68204d38416754bdcd41250f484a49
Stanislav <adm.job@yandex.ru>
2026-03-02 17:38:36 +0500

    edits enemy selector

--- Assets/Scripts/EnemySelector.cs
+++ Assets/Scripts/EnemySelector.cs
@@ -5,39 +5,21 @@ using UnityEngine;
 
 public class EnemySelector : MonoBehaviour
 {
-    //private int _repeatRate = 2;
-
-    //public event Action Generating;
-    //public event Action SpawnGreen;
-    //public event Action SpawnBlue;
-
     [SerializeField]private List<EnemySpawner> _spawner;
-    private EnemySpawner _enemySpawner1;
-    private EnemySpawner _enemySpawner2;
-    private EnemySpawner _enemySpawner3;
 
     private void Start()
     {
-        //StartCoroutine(StartCreation());
-        //_spawner.Add(_enemySpawner1);
-        //_spawner.Add(_enemySpawner2);
-        //_spawner.Add(_enemySpawner3);
-        EnemySpawner[] enemySpawners = FindObjectsByType<EnemySpawner>(FindObjectsSortMode.None);
-        //_spawner.AddRange(enemySpawners);
-        _spawner = new List<EnemySpawner>(enemySpawners);
-        
         StartCoroutine(Creatings());
     }
 
     private IEnumerator Creatings()
     {
-
         while (enabled)
         {
+            int index = UnityEngine.Random.Range(0, _spawner.Count);
+            _spawner[index].StartOneEnemy();
+            
             yield return new WaitForSeconds(1);
-            _spawner[0].StartOneEnemy();
-            _spawner[1].StartOneEnemy();
-            _spawner[2].StartOneEnemy();
         }
     }
 
